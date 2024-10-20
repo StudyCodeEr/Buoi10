3f58189d60ff3ad0998741465bbdd6fd08178005
StudyCodeEr <Nguyenkien7901@gmail.com>
2024-10-20 22:47:18 +0700

    Upload

---
+++ Bai1.js
@@ -0,0 +1,5 @@
+var x = document.getElementsByTagName("p");
+document.getElementById("demo1").innerHTML = 'Thông điệp đầu tiên (index 0) là: ' + x[0].innerHTML;
+
+var x = document.getElementsByClassName("intro");
+document.getElementById("demo2").innerHTML = 'Thông điệp đầu tiên (index 0) là: ' + x[0].innerHTML;
